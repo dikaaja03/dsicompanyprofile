bd38adbef08772987f4c4d4159056b01d9d4d564
dikayow <adikagustiawan730@gmail.com>
2024-07-10 09:45:17 +0700

    menambahkan wa buton

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        keyframes: {
+          shake: {
+            '0%, 100%': { transform: 'translateX(0)' },
+            '25%': { transform: 'translateX(-5px)' },
+            '50%': { transform: 'translateX(5px)' },
+            '75%': { transform: 'translateX(-5px)' },
+          },
+        },
+        animation: {
+          shake: 'shake 0.5s infinite',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
